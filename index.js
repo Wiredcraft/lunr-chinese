3e7c6b1d31019bb5bfe0f53fd8bbb235a03325ff
Oliver.W <ole3021@gmail.com>
2018-04-23 12:40:36 +0800

    init method

--- index.js
+++ index.js
@@ -1,67 +1,96 @@
-const lunr = require('lunr')
-const segment = require('nodejieba')
-const fs = require('fs')
+const lunr = require('lunr');
+const segment = require('nodejieba');
+const fs = require('fs');
 
-const chineseRegx = /[\u4e00-\u9fa5]/
+const chineseRegx = /[\u4e00-\u9fa5]/;
+let config;
 
-const containChinese = obj => {
-  if (!obj) return []
+const containChinese = (obj) => {
+  if (!obj) return [];
 
   if (Array.isArray(obj)) {
-    return obj.every(str => str.toString().trim().match(chineseRegx))
+    return obj.every((str) =>
+      str
+        .toString()
+        .trim()
+        .match(chineseRegx)
+    );
   } else {
-    return obj.toString().trim().match(chineseRegx)
+    return obj
+      .toString()
+      .trim()
+      .match(chineseRegx);
   }
-}
+};
 
-const tokenizChinses = obj => {
-  if (Array.isArray(obj)) return obj.map(t => lunr.utils.asString(t))
+const tokenizChinses = (obj, method) => {
+  if (Array.isArray(obj)) return obj.map((t) => lunr.utils.asString(t));
 
-  const str = obj.toString().trim().toLowerCase()
-  return segment.cut(str, true)
-}
+  const str = obj
+    .toString()
+    .trim()
+    .toLowerCase();
+  return segment[method](str, true);
+};
 
-lunr.trimmer = token => {
-  return token.replace(/^\s+/, '').replace(/\s+$/, '')
-}
+lunr.trimmer = (token) => {
+  return token.replace(/^\s+/, '').replace(/\s+$/, '');
+};
 
-lunr.Pipeline.registerFunction(lunr.trimmer, 'trimmer')
+lunr.Pipeline.registerFunction(lunr.trimmer, 'trimmer');
 
-lunr._tokenizer = lunr.tokenizer
+lunr._tokenizer = lunr.tokenizer;
 
-lunr.tokenizer = obj => {
-  if (!arguments.length || !obj) return []
-  if (containChinese(obj)) return tokenizChinses(obj)
+lunr.tokenizer = (obj, method) => {
+  if (!arguments.length || !obj) return [];
+  if (containChinese(obj)) {
+    //TODO: validate method name
+    const availableMethods = [
+      'cut',
+      'cutAll',
+      'cutHMM',
+      'cutForSearch',
+      'cutSmall'
+    ];
+    method =
+      (availableMethods.indexOf(method) >= 0 && method) ||
+      (config &&
+        availableMethods.indexOf(config.method) >= 0 &&
+        config.method) ||
+      'cut';
+    return tokenizChinses(obj, method);
+  }
 
-  return lunr._tokenizer(obj)
-}
+  return lunr._tokenizer(obj);
+};
 
-lunr.tokenizer.separator = lunr._tokenizer.separator
-lunr.tokenizer.load = lunr._tokenizer.load
-lunr.tokenizer.label = lunr._tokenizer.label
-lunr.tokenizer.registeredFunctions = lunr._tokenizer.registeredFunctions
+lunr.tokenizer.separator = lunr._tokenizer.separator;
+lunr.tokenizer.load = lunr._tokenizer.load;
+lunr.tokenizer.label = lunr._tokenizer.label;
+lunr.tokenizer.registeredFunctions = lunr._tokenizer.registeredFunctions;
 
 lunr.init = (idx, data, path) => {
   if (data) {
-    if (!Array.isArray(data)) return idx
+    if (!Array.isArray(data)) return idx;
 
-    data.map(content => idx.add(content))
+    data.map((content) => idx.add(content));
 
     if (path) {
-      fs.writeFile(path, JSON.stringify(idx), err => {
-        if (err) throw err
+      fs.writeFile(path, JSON.stringify(idx), (err) => {
+        if (err) throw err;
 
-        console.log('>>> Generate Index complete!')
-      })
+        console.log('>>> Generate Index complete!');
+      });
     } else {
-      return idx
+      return idx;
     }
   } else {
-    return lunr.Index.load(idx)
+    return lunr.Index.load(idx);
   }
-}
+};
 
-module.exports = config => {
-  if (config) segment.load(config)
-  return lunr
-}
+module.exports = (conf) => {
+  config = conf;
+  if (config) segment.load(config);
+  return lunr;
+};
